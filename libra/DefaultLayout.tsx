224131660f3ebed3d89613c8fb32171cc9bd4c56
Jon Ambas <jon@jonambas.com>
2023-08-27 13:37:34 -0400

    docs: update default layout (#123)

--- libra/DefaultLayout.tsx
+++ libra/DefaultLayout.tsx
@@ -1,12 +1,35 @@
 import { FC, PropsWithChildren } from 'react';
 import './index.css';
 import { css } from '@styles/css';
+import { Link, Stack, Text } from '../packages/react/src';
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+  if (!children) {
+    return (
+      <div>
+        <Stack>
+          <Text element="h1" looksLike="h2">
+            @sweatpants/react
+          </Text>
+          <Text element="p" looksLike="p">
+            See{' '}
+            <Link
+              href="https://github.com/jonambas/sweatpants"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              github.com/jonambas/sweatpants
+            </Link>
+            .
+          </Text>
+        </Stack>
+      </div>
+    );
+  }
+
   return (
     <div
       className={css({
-        fontFamily: 'sans',
         padding: '4'
       })}
     >
