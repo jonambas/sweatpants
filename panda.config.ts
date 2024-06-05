5809af6a87566ad5fe6d212354c8be5d4a8c9e2a
jonambas <jon@jonambas.com>
2024-06-05 19:44:13 -0400

    build: add workaround

--- panda.config.ts
+++ panda.config.ts
@@ -3,12 +3,11 @@ import preset from './packages/panda-preset/src';
 
 export default defineConfig({
   preflight: true,
-  include: [
-    './libra/**/*.tsx',
-    './packages/react/**/*.tsx',
-  ],
+  include: ['./libra/**/*.tsx', './packages/react/**/*.tsx'],
   exclude: ['node_modules', 'dist'],
   presets: [preset],
+  outExtension: 'js',
+  clean: true,
 
   // Library options
   outdir: 'styles',
